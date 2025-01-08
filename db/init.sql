ce43509f1d68d7a49f70d0054833087f527911bd
IldartDiyar <diyar.ildart@gmail.com>
2025-01-08 18:24:28 +0500

    fix db

--- db/init.sql
+++ db/init.sql
@@ -1,26 +1,14 @@
-CREATE TABLE Surveys (
+CREATE TABLE respondents (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(20),
-    created_at TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Questions (
+CREATE TABLE answers (
     id SERIAL PRIMARY KEY,
-    question TEXT
+    respondent_id INT REFERENCES respondents(id) ON DELETE CASCADE,
+    question_text TEXT NOT NULL,
+    answer_text TEXT NOT NULL,,
+    CONSTRAINT fk_respondent FOREIGN KEY (respondent_id) REFERENCES respondents(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Respondents (
-    id SERIAL PRIMARY KEY,
-    time TIMESTAMP
-);
-
-CREATE TABLE Answers (
-    survey_id INT NOT NULL,
-    question_id INT NOT NULL,
-    respondent_id INT NOT NULL,
-    answer_text TEXT,
-    PRIMARY KEY (survey_id, question_id, respondent_id),
-    CONSTRAINT fk_survey FOREIGN KEY (survey_id) REFERENCES Surveys (id),
-    CONSTRAINT fk_question FOREIGN KEY (question_id) REFERENCES Questions (id),
-    CONSTRAINT fk_respondent FOREIGN KEY (respondent_id) REFERENCES Respondents (id)
-);
+CREATE INDEX idx_respondent_id ON answers (respondent_id);
