3c3c48fd8eea6eaac1550c684aab814e1946fcf3
IldartDiyar <diyar.ildart@gmail.com>
2025-01-08 18:31:08 +0500

    fix sql

--- db/init.sql
+++ db/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE answers (
     id SERIAL PRIMARY KEY,
     respondent_id INT REFERENCES respondents(id) ON DELETE CASCADE,
     question_text TEXT NOT NULL,
-    answer_text TEXT NOT NULL,,
+    answer_text TEXT NOT NULL,
     CONSTRAINT fk_respondent FOREIGN KEY (respondent_id) REFERENCES respondents(id) ON DELETE CASCADE
 );
 
